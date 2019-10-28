bb0a3f8de801dff61e17565cdfb783861a0fa5e8
Joshua Thornton <joshua_thornton1@homedepot.com>
2019-10-28 13:01:43 -0400

    more integration tests

--- pkg/integration_test.go
+++ pkg/integration_test.go
@@ -4,6 +4,7 @@ package tms
 
 import (
 	"github.com/stretchr/testify/assert"
+	"sync"
 	"testing"
 )
 
@@ -40,18 +41,18 @@ func TestMigrateDatabase(t *testing.T) {
 	}
 }
 
-func TestCreatingStrainInDB(t *testing.T) {
+func TestCreatingStrainInDBUpdatesName(t *testing.T) {
 	assert := assert.New(t)
 
 	tests := []struct {
 		name string
 	}{
 		{"foo"},
+		{"bar"},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// TODO: we really need a number generator for these tests that hands out new numbers and does not reuse them
 			var ref uint = Unique.Next()
 			in := Strain{Name: tt.name, ReferenceID: ref}
 			// plug in the db
@@ -62,8 +63,123 @@ func TestCreatingStrainInDB(t *testing.T) {
 
 			out := Strain{ReferenceID: ref}
 			// populate new object
-			TestDB.Model(&out).First(&out)
+			TestDB.Where(&out).First(&out)
 			assert.Equal(tt.name, out.Name)
+		})
+	}
+}
+
+func TestCreatingStrainInDBUpdatesRace(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		race string
+	}{
+		{"indica"},
+		{"sativa"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.race, func(t *testing.T) {
+			var ref uint = Unique.Next()
+			in := Strain{Name: tt.race, Race: tt.race, ReferenceID: ref}
+			// plug in the db
+			in.DB = TestDB
+			// set the reference to verify in the new object
+			in.ReferenceID = ref
+			assert.Nil(in.CreateInDB())
+
+			out := Strain{ReferenceID: ref}
+			// populate new object
+			TestDB.Where(&out).First(&out)
+			assert.Equal(tt.race, out.Race)
+		})
+	}
+}
+
+func TestSavingStrainUpdatesName(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+
+	tests := []struct {
+		name string
+	}{
+		{"foo"},
+		{"bar"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var ref uint = Unique.Next()
+			in := Strain{Name: "first", ReferenceID: ref}
+			// plug in the db
+			in.DB = TestDB
+			// set the reference to verify in the new object
+			in.ReferenceID = ref
+			assert.Nil(in.CreateInDB())
+
+			in.Name = tt.name
+			in.SaveInDB()
+
+			out := Strain{ReferenceID: ref}
+			// populate new object
+			TestDB.Where(&out).First(&out)
+			assert.Equal(tt.name, out.Name)
+		})
+	}
+}
+
+func TestGettingStrainFromFlavorsFromDBByID(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+
+	tests := []struct {
+		name       string
+		strain     Strain
+		expFlavors []string
+	}{
+		{
+			"one_flavor",
+			Strain{
+				Name:    "foo",
+				Race:    "xyz",
+				Flavors: []Flavor{{Name: "yummy"}},
+			},
+			[]string{"yummy"},
+		},
+		{
+			"many_flavors",
+			Strain{
+				Name:    "foo",
+				Race:    "xyz",
+				Flavors: []Flavor{{Name: "yummy"}, {Name: "tasty"}},
+			},
+			[]string{"yummy", "tasty"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.strain.Name, func(t *testing.T) {
+			ref := Unique.Next()
+			tt.strain.DB = TestDB
+
+			// push strain with flavors in to DB
+			tt.strain.ReferenceID = ref
+			assert.Nil(tt.strain.CreateInDB())
+
+			out := Strain{ReferenceID: ref}
+			out.DB = TestDB
+			flavors, err := out.FlavorsFromDBByID()
+			assert.Nil(err)
+			for _, expected := range tt.expFlavors {
+				var match bool
+				for _, f := range flavors {
+					if f.Name == expected {
+						match = true
+					}
+				}
+				assert.True(match, "did not find expected flavor %s in returned flavors", expected)
+			}
 		})
 	}
 }
